b965de08d9ca5b7ad7571d98e13ded18df62664b
p4535992 <tentimarco0@gmail.com>
2020-01-05 22:13:42 +0100

    Update

--- alfresco-allinone-abd/alfresco-allinone-abd-platform-jar/src/script/addAspectAction.js
+++
@@ -1,7 +0,0 @@
-logger.log("Start Add Aspect Action:" + aspectName);
-//var aspectName = "sc:clientRelated"; 
-var aspectName = args[0];
-var addAspectAction = actions.create("add-features"); 
-addAspectAction.parameters["aspect-name"] = aspectName; 
-addAspectAction.execute(document); 
-logger.log("End Add Aspect Action:" + aspectName);
